9b8cfce846eaf8476aed04370bbc5112b72ba433
Natasha Ilievska <ilievska28@gmail.com>
2020-10-28 20:47:17 +0100

    Added MUI theme and override primary and secondary colors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import Auth from './components/Auth/Auth';
 import Layout from './components/Layout/Layout';
 import Welcome from './components/Welcome';
 import Games from './components/Games';
 
+import * as CONST from './constants';
+
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: CONST.THEME_COLORS.primary,
+      secondary: CONST.THEME_COLORS.secondary,
+    },
+  });
   return (
-    <Layout>
-      <Switch>
-        <Route path="/auth" component={Auth}></Route>
-        <Route path="/games" component={Games}></Route>
-        <Route path="/" exact component={Welcome}></Route>
-      </Switch>
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Switch>
+          <Route path="/auth" component={Auth}></Route>
+          <Route path="/games" component={Games}></Route>
+          <Route path="/" exact component={Welcome}></Route>
+        </Switch>
+      </Layout>
+    </ThemeProvider>
   );
 }
 
---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+export const THEME_COLORS = {
+  primary: {
+    light: '#CEDDE7',
+    main: '#88B5C4',
+    dark: '#7AA2B0',
+  },
+  secondary: {
+    light: '#EAD7D9',
+    main: '#D6B8BD',
+    dark: '#C0A5AA',
+  },
+};
