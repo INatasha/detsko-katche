945345c59674ae5597e1634c1213076cf6407a7a
Natasha Ilievska <ilievska28@gmail.com>
2020-10-29 10:39:41 +0100

    Make Memory and Settings pages consistent

--- src/components/Games/Memory/MemoryWrapper.js
+++ src/components/Games/Memory/MemoryWrapper.js
@@ -6,12 +6,15 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 import * as CONST from '../../../constants';
 
 const useStyles = makeStyles((theme) => ({
-  div: {
-    margin: theme.spacing(8),
+  wrapperDiv: {
+    marginTop: theme.spacing(8),
+  },
+  paper: {
     padding: theme.spacing(6),
   },
   typography: {
@@ -23,10 +26,8 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  gridItem: {
-    padding: theme.spacing(6),
-  },
 }));
+
 function MemoryWrapper() {
   const [gameTheme, setGameTheme] = useState(null);
   const [currentImg, setCurrentImg] = useState(
@@ -36,47 +37,49 @@ function MemoryWrapper() {
   const showChooseThemeView = gameTheme ? false : true;
   const classes = useStyles();
 
-  function renderChooseThemeView() {
-    return (
-      <Grid container justify="center" alignItems="center">
-        <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom>
-            Одбери категорија
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            gutterBottom
-            className={classes.typography}
-          >
-            Со избирање на некоја од категориите долу, играта Меморија ќе биде
-            со картички од соодветната категорија!
-          </Typography>
-        </Grid>
-        <Grid item xs={12} container spacing={4} justify="space-evenly">
-          {themes.map((theme) => (
-            <Grid key={theme.KEY} item md sm={12} xs={12}>
-              <Card
-                style={
-                  theme.LOCKED ? { opacity: 0.5 } : { background: theme.COLOR }
-                }
-              >
-                <CardActionArea
-                  className={classes.cardContent}
-                  disabled={theme.LOCKED}
-                  onClick={() => {
-                    if (!theme.LOCKED) setGameTheme(theme);
-                  }}
+  return (
+    <div className={classes.wrapperDiv}>
+      <Paper className={classes.paper}>
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h4" gutterBottom>
+              Одбери категорија
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              className={classes.typography}
+            >
+              Со избирање на некоја од категориите долу, играта Меморија ќе биде
+              со картички од соодветната категорија!
+            </Typography>
+          </Grid>
+          <Grid item xs={12} container spacing={4} justify="space-evenly">
+            {themes.map((theme) => (
+              <Grid key={theme.KEY} item md sm={12} xs={12}>
+                <Card
+                  style={
+                    theme.LOCKED
+                      ? { opacity: 0.5 }
+                      : { background: theme.COLOR }
+                  }
                 >
-                  <Typography variant="h4">{theme.DISPLAY}</Typography>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))}
+                  <CardActionArea
+                    className={classes.cardContent}
+                    disabled={theme.LOCKED}
+                    onClick={() => {
+                      if (!theme.LOCKED) setGameTheme(theme);
+                    }}
+                  >
+                    <Typography variant="h4">{theme.DISPLAY}</Typography>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
-      </Grid>
-    );
-  }
-
-  return <div className={classes.div}>{renderChooseThemeView()}</div>;
+      </Paper>
+    </div>
+  );
 }
 export default MemoryWrapper;
